4967341568e8f63c805429ed9a42ac87b795b07a
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-02-11 17:14:16 +0530

    Updated registry server controller

--- src/controller/registry_server_controller.py
+++ src/controller/registry_server_controller.py
@@ -1,4 +1,10 @@
+from src.service.registry_server_service import RegistryServerService
+
+
 class RegistryServerController:
 
+    def __init__(self):
+        self.service = RegistryServerService()
+
     def get_all_server(self):
-        pass
+        return self.service.get_all_server()
