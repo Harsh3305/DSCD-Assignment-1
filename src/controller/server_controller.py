df0d492b6d30995e8b5f9c36c9a09b5fe81e960c
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-02-11 17:02:05 +0530

    Linked server controller with server service

--- src/controller/server_controller.py
+++ src/controller/server_controller.py
@@ -1,14 +1,14 @@
-from src.model.server import Server
+from src.service.server_service import ServerService
 
 
 class ServerController:
 
     def __init__(self, port: int, name: str):
-        self.server = Server(name=name, address="localhost:" + str(port))
+        self.service = ServerService(name=name, address="localhost:" + str(port))
         # Call register server of registry server
 
     def get_all_client(self):
-        pass
+        return self.service.get_all_client()
 
     def join_server(self, server_address: str):
-        pass
+        return self.service.join_server(server_address=server_address)
