6cbd5e1ae2e17d2d49105c82f458858de13292d1
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-02-11 21:56:24 +0530

    Added registry server feature

--- src/repository/registry_server_repository.py
+++ src/repository/registry_server_repository.py
@@ -7,5 +7,7 @@ class RegistryServeRepository:
         self.registry_server = RegistryServer()
 
     def get_all_server(self):
-        ## TODO:
-        pass
+        self.registry_server.get_all_server()
+
+    def register_server(self, server_address: str):
+        self.registry_server.register_address(server_address=server_address)
